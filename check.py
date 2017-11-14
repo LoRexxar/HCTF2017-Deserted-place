86639a5b2142d86222978b6c7fb6eed00432ba21
LoRexxar <lorexxar@gmail.com>
2017-11-14 11:18:28 +0800

    first commit

---
+++ check.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import selenium
+from selenium import webdriver  
+from selenium.webdriver.common.keys import Keys  
+from selenium.common.exceptions import WebDriverException
+import os 
+import time 
+import requests
+
+# only for vps
+# from pyvirtualdisplay import Display
+
+# display = Display(visible=0, size=(800,800))
+# display.start()
+
+
+url = "http://desert.hctf.com"
+username = "hctf_admin_LoRexxar2e23"
+password = "password"
+
+
+while 1:
+	try:
+		
+		# only for windows
+		# chromedriver = "C:\Users\lorex\AppData\Local\Google\Chrome\Application\chromedriver.exe"  
+		# os.environ["webdriver.chrome.driver"] = chromedriver  
+		# browser = webdriver.Chrome(chromedriver) 
+		
+		browser = webdriver.Firefox()
+
+		print "[INFO] start new round..."
+
+		browser.set_page_load_timeout(10)
+		browser.set_script_timeout(10)
+
+		out_url = url + '/logout.php'
+		browser.get(out_url)
+
+		a_url = url+'/login.php'	
+		browser.get(a_url)
+		time.sleep(1)
+
+		elem = browser.find_element_by_name("user")
+		elem.clear()
+		elem.send_keys(username)
+		elem = browser.find_element_by_name("pass")
+		elem.clear()
+		elem.send_keys(password)
+		elem = browser.find_element_by_name("submit")
+		elem.click()
+
+		l_url = url+'/api/getmess.php'
+		browser.get(l_url)
+		while 1:
+			try:
+				browser.switch_to_alert().accept()
+			except selenium.common.exceptions.NoAlertPresentException:
+				break
+		source = browser.page_source
+
+		if "Nothing" not in source:
+			print time.strftime("%Y-%m-%d %X", time.localtime())
+			print "[info] Reading now..."
+			time.sleep(4)
+
+			d_url = url+'/api/clearmess.php'
+			browser.get(l_url)
+
+			browser.quit()
+			time.sleep(1)
+
+		if "This is not where you should come" in source:
+			print time.strftime("%Y-%m-%d %X", time.localtime())
+			print "[info] bot admin check error..."
+			browser.quit()
+			time.sleep(2)
+
+		else:
+			print time.strftime("%Y-%m-%d %X", time.localtime())
+			print "[info] no unread messages..."
+			browser.quit()
+			time.sleep(2)
+			# exit(0)
+
+	except Exception as e: 
+		print "[error] "+str(e)
+		time.sleep(2)
+		browser.quit()
---
+++ class/config.php
@@ -0,0 +1,8 @@
+<?php
+	$config['hostname'] = 'localhost';
+	$config['username'] = 'hctf2017';
+	$config['password'] = 'nTa2vaUOpjCGFlJQ';
+	$config['database'] = 'hctf2017_web2';
+
+	$db = new mysqli($config['hostname'],$config['username'],$config['password'],$config['database']);
+?>
